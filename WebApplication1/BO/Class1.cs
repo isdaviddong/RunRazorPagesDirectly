b8914d1f7d35caea4285520345a1a12255d48c15
David@SurfacePro4 <isdaviddong@gmail.com>
2017-10-30 14:08:12 +0800

    直接使用WebPages without controller

---
+++ WebApplication1/BO/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BO
+{
+    public class Class1
+    {
+        public string MethodA()
+        {
+            return DateTime.Now.ToString();
+        }
+    }
+}
